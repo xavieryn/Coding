93a613fced1cf1adbcd29fcfc2b3dd722a8cd068
xavieryn <xavier.nishikawa@gmail.com>
2022-12-23 10:07:49 -0800

    Starting to mess with Dataframes instead of 2d array

--- FredHutchinson/dataframe.py
+++ FredHutchinson/dataframe.py
@@ -1,5 +1,36 @@
+import pandas as pd
+
 def main():
-    print('hello')
+  
+
+  header = [
+  'Study', 'Treatment', 'Virus', 'VirusID', 'AssayID', 'Lab', 'Poscrit'
+  ]
+  df = pd.read_csv('CSV/220708_Edit.csv')
+
+  print(df.columns)
+  print(len(df.index))
+  df.columns = df.iloc[0]
+  df = df[1:]
+  print(df.columns)
+  print(len(df.index))
+  
+
+  addTreatment(df)
+
+  print(df)
+
+def addTreatment(data):
+  # asks for treatment like Placebo or VRC01, and will add the treatment to each row
+  treatment = input("Enter the treatment: ")
+  data.insert(1,'Treatment', treatment )
+
+def addLab(data):
+  #asks for lab name and will add the lab to each row
+  labName = input("Enter the Lab: ")
+  data.insert(2,'Lab', labName )
+
+
 
 if __name__ == '__main__':
   main()
